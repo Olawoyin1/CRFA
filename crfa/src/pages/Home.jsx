1bf5373fbaa8d06406450fe843bc9dda93a8fd66
Olawoyin <yustee2017@gmail.com>
2024-04-01 11:51:17 +0100

    Adeed About section to the home page

--- crfa/src/pages/Home.jsx
+++ crfa/src/pages/Home.jsx
@@ -1,3 +1,8 @@
+import { Link } from "react-router-dom";
+
+
+
+
 const Home = () => {
   return (
     <>
@@ -14,6 +19,44 @@ const Home = () => {
                 className="img-fluid "
                 alt="lll"
               /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="about_us py-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="about_image">
+                <img src="../../Images/about.jpg" alt="about us Img" />
+              </div>
+            </div>
+            <div className="col-md-6 mt-3 mt-md-0">
+              <h2 className="fw-bold mb-3 dash">About CRFA</h2>
+              <div className="about_text">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                  aperiam id labore fugiat. Id dolor ea odit odio, vero quo sed
+                  hic nesciunt corporis autem ipsam eligendi ratione commodi
+                  fugiat. <br />
+                  <br />
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Facilis quos nulla placeat, nobis nesciunt alias optio illum,
+                  tempore maxime officiis excepturi, non exercitationem facere
+                  officia sunt earum rerum. At, sequi!
+                  <br />
+                  <br />
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est,
+                  ipsum quaerat officia atque consequuntur consequatur dicta
+                  sint itaque rem eveniet, excepturi blanditiis beatae, dolores
+                  labore modi vero delectus? Vel, ut!
+                </p>
+                <Link className="button" to="about">
+                  More about us
+                </Link>
+              </div>
             </div>
           </div>
         </div>
