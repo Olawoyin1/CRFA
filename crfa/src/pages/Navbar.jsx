58de54faa7a329f56a655eaf048b26aea5cbaed2
Olawoyin <yustee2017@gmail.com>
2024-04-01 03:17:28 +0100

    Navbar created with Navlinks - News, Players, About and Contact

--- crfa/src/pages/Navbar.jsx
+++ crfa/src/pages/Navbar.jsx
@@ -1,8 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (<>
-  </>
+  return (
+    <div>
+      <nav className="navbar container navbar-expand-lg">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand d-flex align-items-center">
+            <img
+              src="../Images/crfa_logo.png"
+              className="img-fluid logo-img"
+              alt=""
+            />
+            <h2 className="fw-bold mt-2">CRFA</h2>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 gap-3 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link fw-semibold" to="/news">
+                  NEWS
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link fw-semibold" to="/about">
+                  ABOUT US
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link fw-semibold" to="/players">
+                  PLAYERS
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link fw-semibold" to="/contact">
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
