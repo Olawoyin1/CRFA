86668295f7ca57b5947ff93d7c1807eb00229063
Olawoyin <yustee2017@gmail.com>
2024-04-01 11:47:59 +0100

    Hero section created - with Hero Text only

--- crfa/src/pages/Home.jsx
+++ crfa/src/pages/Home.jsx
@@ -1,8 +1,24 @@
-
-
 const Home = () => {
-  return (<>
-  </>
+  return (
+    <>
+      <div className="hero">
+        <div className="container">
+          <div className="d-flex">
+            <div className="hero-content">
+              <h1 className="fw-bold text-white">CLASSIC ROYALS FA</h1>
+              <p className="text-white">Win the DAY, Win the GAME</p>
+            </div>
+            <div className="hero-img">
+              {/* <img
+                src="../../Images/hero.png"
+                className="img-fluid "
+                alt="lll"
+              /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
